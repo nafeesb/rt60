46207621e4d4c79ce056918532cb947d4d60f146
Nafees Bin Zafar <github@nafees.net>
2016-05-08 09:23:04 +0800

    Initial Commit

---
+++ rt60/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rt60
+//
+//  Created by Nafees Bin Zafar on 5/8/16.
+//  Copyright (c) 2016 Nafees Bin Zafar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
